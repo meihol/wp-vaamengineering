41e5d37204638e99639c85b73723f84c1d9957f6
Jenish Rana <jenish.r@gatisofttech.com>
2026-03-23 06:43:34 +0000

    Edit wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -28,16 +28,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u731710703_vaAMEngINeRnGD' );
+define( 'DB_NAME', 'wp_vaamengineering_d' );
 
 /** Database username */
-define( 'DB_USER', 'u731710703_vaAMEngINeRnGU' );
+define( 'DB_USER', 'wp_vaamengineering_u' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Fp:[3E3iP]6' );
+define( 'DB_PASSWORD', 'b3%.ndrD}q' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', 'ec2-43-205-168-50.ap-south-1.compute.amazonaws.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
